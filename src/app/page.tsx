0e8474bade4917b7654c02f215785418a8737d56
Aur316 <kristianfeher86@gmail.com>
2025-04-17 11:04:35 +0200

    fix: prettier ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Home </div>;
+  return (
+    <div className="flex justify-center rounded-md bg-black p-7 font-bold text-black">
+      Home
+    </div>
+  );
 }
