76f7d57c629ac6ddce5bd94b571043a582e3a1c8
Aur316 <kristianfeher86@gmail.com>
2025-04-28 19:01:25 +0200

    feat: implement react query with react query devtools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import { PropsWithChildren } from 'react'
 
 import { ToastProvider } from '../components'
-import { I18nProvider, StoreProvider } from '../providers'
+import { I18nProvider, ReactQueryProvider, StoreProvider } from '../providers'
 import './globals.css'
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
-        <I18nProvider>
-          <StoreProvider>
-            {children}
-            <ToastProvider />
-          </StoreProvider>
-        </I18nProvider>
+        <ReactQueryProvider>
+          <I18nProvider>
+            <StoreProvider>
+              {children}
+              <ToastProvider />
+            </StoreProvider>
+          </I18nProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   )
