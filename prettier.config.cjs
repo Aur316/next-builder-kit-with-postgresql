c9aca633bd54c1dc38b368584d907afa3bd3bbae
Aur316 <kristianfeher86@gmail.com>
2025-04-28 17:12:36 +0200

    feat: import orders and base-ui-element pattern

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
   singleQuote: true,
   semi: false,
-  tailwindConfig: "./tailwind.config.ts",
-};
+  importOrder: [
+    '^react$', // first React
+    '^next', // then next
+    '^@?\\w', // then third-party packages (node_modules)
+    '^@components', // your alias imports
+    '^@utils', // other alias imports
+    '^[./]', // relatív imports
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  tailwindConfig: './tailwind.config.ts',
+}
--- src/components/base-ui-elements/base-button.tsx
+++ src/components/base-ui-elements/base-button.tsx
@@ -1,3 +1,24 @@
-export function BaseButton() {
-  return <button className="relative flex bg-red-500"></button>
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
+
+import { twMerge } from 'tailwind-merge'
+
+interface BaseButtonProps
+  extends PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>> {
+  text?: string
+  className?: string
+}
+
+export function BaseButton({
+  children,
+  text,
+  className,
+  ...props
+}: BaseButtonProps) {
+  const baseStyle = 'px-3 py-2'
+  return (
+    <button className={twMerge(baseStyle, className)} {...props}>
+      {text}
+      {children}
+    </button>
+  )
 }
