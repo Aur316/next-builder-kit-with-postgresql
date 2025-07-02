a5596b996b6f1e3f21e0e7da53beaf481c825a21
Aur316 <kristianfeher86@gmail.com>
2025-07-02 14:05:08 +0200

    refactor: remove unnecessary things from store provider

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,16 @@
 'use client'
 
+import { use } from 'react'
+
 import { useTranslation } from 'react-i18next'
 
 import { Button, SpinnableBorderWrapper } from '../../components'
+import { StoreContext } from '../../providers'
 
 export default function AboutPage() {
   const { t } = useTranslation()
+  const { storeNumber } = use(StoreContext)
+
   const viewSource = () => {
     window.open('https://github.com/Aur316/simple-model', '_blank')
   }
@@ -19,7 +24,10 @@ export default function AboutPage() {
         <p className="text-primary-midnight-blue-100">
           {t('aboutPage.description2')}
         </p>
-
+        <p className="bg-primary-midnight-blue-800 text-primary-midnight-blue-200 rounded-lg p-4">
+          {t('store.context.storeMessage')}
+          <span className="text-primary-midnight-blue-100">{storeNumber}</span>
+        </p>
         <div className="flex justify-center">
           <SpinnableBorderWrapper isSpinning baseStyle="rounded-2xl">
             <Button
