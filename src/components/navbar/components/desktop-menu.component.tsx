59b9d350f86c1210238a9c2b0a31f6647981af4d
Aur316 <kristianfeher86@gmail.com>
2025-07-03 12:11:58 +0200

    refactor: extracted routeitems from the components so that it only needs to be calculated once

--- src/components/navbar/components/desktop-menu.component.tsx
+++ src/components/navbar/components/desktop-menu.component.tsx
@@ -1,31 +1,13 @@
 'use client'
 
-import { memo, useMemo } from 'react'
-
-import Link from 'next/link'
-
-import { useTranslation } from 'react-i18next'
-
-import { Route } from '../../../types'
+import { JSX, memo } from 'react'
 
 interface DesktopMenuProps {
-  routes: Array<Route>
+  routeItems: JSX.Element
 }
 
 export const DesktopMenu = memo(function DesktopMenu({
-  routes,
+  routeItems,
 }: DesktopMenuProps) {
-  const { t } = useTranslation()
-
-  const routeItems = useMemo(
-    () =>
-      routes.map((route) => (
-        <Link key={route.path} href={route.path}>
-          {t(route.name)}
-        </Link>
-      )),
-    [routes, t],
-  )
-
-  return <div className="hidden gap-6 md:flex">{routeItems}</div>
+  return routeItems
 })
